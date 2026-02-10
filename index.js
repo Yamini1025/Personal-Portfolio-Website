e366260952a4078154065e9705f04587cb817dfa
Yamini1025 <yamini.karthik06@gmail.com>
2026-02-09 22:42:08 -0800

    updated html & js

---
+++ index.js
@@ -0,0 +1,5 @@
+const date = new Date();
+const year = date.getFullYear();
+
+const footer = document.querySelector(".footer-text");
+footer.textContent = "© " + year + " Yamini Karthik. All rights reserved.";
