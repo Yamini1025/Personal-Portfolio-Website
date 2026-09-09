49ee2edc8ec3d9885172103197c8f0b76372a03c
Yamini1025 <yamini.karthik06@gmail.com>
2026-09-09 10:49:33 -0700

    rebuilding portfolio - start with html

--- index.js
+++
@@ -1,5 +0,0 @@
-const date = new Date();
-const year = date.getFullYear();
-
-const footer = document.querySelector(".footer-text");
-footer.textContent = "© " + year + " Yamini Karthik. All rights reserved.";
