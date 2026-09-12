d86d4f15ddbfd7b10ac36fb38a3674afeaf39f8e
Yamini1025 <yamini.karthik06@gmail.com>
2026-09-12 00:16:33 -0700

    update home page

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const words = ["AI agents.", "ML systems.", "full-stack apps."];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const targetEl = document.getElementById("typewriter-text");
+function typeEffect() {
+  const currentWord = words[wordIndex];
+
+  if (isDeleting) {
+    targetEl.textContent = currentWord.substring(0, charIndex - 1);
+    charIndex--;
+  } else {
+    targetEl.textContent = currentWord.substring(0, charIndex + 1);
+    charIndex++;
+  }
+
+  let typeSpeed = isDeleting ? 40 : 40;
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    typeSpeed = 2000; 
+    isDeleting = true;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+    typeSpeed = 400; 
+  }
+
+  setTimeout(typeEffect, typeSpeed);
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect);
