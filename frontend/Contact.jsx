862e07f27ec2b059f8deeac5277494883da3b4e6
Yamini1025 <yamini.karthik06@gmail.com>
2026-09-18 23:16:27 -0700

    started contact page

--- frontend/Contact.jsx
+++ frontend/Contact.jsx
@@ -5,10 +5,19 @@ export default function Contact() {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     return (
-        <h2>Happy to connect!</h2>
-        <div>
-            <p>Name</p>
-        </div>
+        <section>
+            <h2>Happy to connect!</h2>
+            <div>
+                <p>Name</p>
+
+            </div>
+            <div class="hero-buttons">
+                    <a href="mailto:yamini.karthik06@gmail.com" className="contact-button">Email</a>
+                    <a href="https://github.com/Yamini1025" className="contact-button">GitHub</a>
+                    <a href="https://www.linkedin.com/in/yamini-karthik/" className="contact-button">LinkedIn</a>
+                </div>
+        </section>
+        
     )
 
 }
